fcb37f0c1c80524d888e988787ffac56c243f791
TheDedas <andrefm.freire@gmail.com>
2015-06-12 18:29:06 +0100

    Adds a shell script to compile and run the server.

---
+++ gen/split_piece.go
@@ -0,0 +1,15 @@
+package gen
+
+ import (
+//   "gogs.pierreqr.fr/doodloo/communitrix/array"
+   "gogs.pierreqr.fr/doodloo/communitrix/logic"
+//   "gogs.pierreqr.fr/doodloo/communitrix/util"
+//   "math"
+//   "math/rand"
+ )
+
+func SplitIntoPieces(input *logic.Piece, numPieces int, minPieceSize int) []*logic.Piece {
+//minX, maxX, minY, maxY, minZ, maxZ = -1,-1,-1,-1,-1,-1
+
+return make([]*logic.Piece, 2)
+}
