f9a2a0ffca2a54822754dac20519df4b028e5029
HERAULT-MARTIN Pierre <HicksCorp@GMail.com>
2015-06-18 15:49:21 +0100

    Tweaks the pieces splitting for a more feasible chalenge.

--- combat.go
+++ combat.go
@@ -249,7 +249,7 @@ func (this *Combat) Prepare() (*cbt.Start, bool) {
 	}
 	log.Debug("  - Target: Cells %d, Size: %d", target.Size, len(target.Content))
 
-	pieces, ok := gen.NewPieceSplitter().Run(target, 4)
+	pieces, ok := gen.NewPieceSplitter().Run(target, 8)
 	if !ok {
 		log.Warning("Something went wrong during pieces generation.")
 		return nil, false
